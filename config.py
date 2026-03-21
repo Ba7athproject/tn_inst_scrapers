79454d6c040be50c4a87c0331a65af431fb2ffec
Ba7athproject <ba7ath.project@gmail.com>
2026-03-21 22:47:31 +0000

    Fix Playwright sur Streamlit Cloud

--- config.py
+++ config.py
@@ -7,7 +7,12 @@ def initialize_system():
     # Correction de l'erreur NotImplementedError pour Playwright sur Windows
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    else:
+        # Installation des binaires Chromium sur Streamlit Cloud au premier lancement
+        if not os.path.exists(os.path.expanduser("~/.cache/ms-playwright")):
+            os.system("playwright install chromium")
     
     # Autres initialisations possibles (logs, dossiers temporaires, etc.)
     if not os.path.exists("debug"):
         os.makedirs("debug", exist_ok=True)
+
